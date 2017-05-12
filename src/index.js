751c97baae07afa575e924a55e7c65a0c808b6fe
boadmin <erik@bodigiti.com>
2017-05-12 16:49:27 -0400

    React-Router-- add as instructed by Katz, but not working.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+// Components Import
 import App from './components/App';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 
 
 ReactDOM.render(
-  <div>App</div>,
+  // Setup Router
+  // Use push, pop, and replace to redirect...
+  <Router path="/" history={browserHistory}>
+    // Set paths
+    <Route path="/app" component={App} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+  </Router>,
   document.getElementById('root')
 );
