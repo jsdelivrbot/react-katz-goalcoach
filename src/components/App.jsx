b3edfc8a131a4c7d2236a0f74fba6ed8334d05fa
boadmin <erik@bodigiti.com>
2017-05-12 15:08:22 -0400

    Initial commit.

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+// Connect component to STORE: 1 of 
+// { connect } function works by hooking up two functions to react component,
+// 1) it first connects a "maps states to props" function and a
+// 2) "maps dispatch to props" function 
+import { connect } from 'react-redux';
+
+
